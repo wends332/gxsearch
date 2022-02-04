28e5a7152bc13f98d2d918039e40397f339e528a
wendongsong <wendongsong@qtrade.com.cn>
2022-02-04 16:43:27 +0800

    代码提交

---
+++ search/src/test/java/com/wen/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wen.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
